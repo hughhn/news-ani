081ae0b7d225a7049b4c3ad18620108bc8f1cc92
Hugo Nguyen <hugo@scribd.com>
2015-04-14 17:01:45 -0700

    delegate to Analyzer webservice

--- app/controllers/Feed.java
+++ app/controllers/Feed.java
@@ -1,10 +1,11 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
-import com.rometools.rome.feed.synd.SyndLink;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
+import com.typesafe.config.ConfigFactory;
 import play.*;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -13,24 +14,46 @@ import securesocial.core.java.SecureSocial;
 import securesocial.core.java.SecuredAction;
 import services.User;
 import twitter4j.*;
-import twitter4j.Logger;
 import views.html.feed;
 
-import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
+
+import play.libs.ws.*;
+import play.libs.F.Promise;
 
 /**
  * Created by hugh_sd on 4/8/15.
  */
 public class Feed extends Controller {
+
+    public static String baseUrl = null;
     public static play.Logger.ALogger logger = play.Logger.of("application.controllers.Feed");
 
+    public static class Item {
+        public SyndEntry entry;
+        public String metadata;
+
+        public Item(SyndEntry entry, String metadata) {
+            this.entry = entry;
+            this.metadata = metadata;
+        }
+    }
+
+    public static String getApiBaseUrl() {
+        if (baseUrl != null) {
+            return baseUrl;
+        }
+
+        baseUrl = ConfigFactory.load().getString("analyzer.baseurl");
+        logger.debug("baseUrl = " + baseUrl);
+        return baseUrl;
+    }
+
     public static Twitter getTwitterInstance() {
-// The factory instance is re-useable and thread safe.
+        // The factory instance is re-useable and thread safe.
         TwitterFactory factory = new TwitterFactory();
         Twitter twitter = new TwitterFactory().getInstance();
         twitter.setOAuthConsumer(Play.application().configuration().getString("securesocial.twitter.consumerKey"),
@@ -87,15 +110,9 @@ public class Feed extends Controller {
             URL feedUrl = new URL("https://news.ycombinator.com/rss");
             SyndFeedInput input = new SyndFeedInput();
             SyndFeed feed = input.build(new InputStreamReader(feedUrl.openStream()));
-
             //logger.debug("feed: \n" + feed);
-            entries = feed.getEntries();
 
-
-            for (SyndEntry entry : entries) {
-                logger.debug("@" + entry.getTitle() + " - " + entry.getLink());
-            }
-            return entries;
+            return feed.getEntries();
         } catch (FeedException | IOException e) {
             e.printStackTrace();
             logger.error("getSyndFeeds() Exception");
@@ -104,9 +121,28 @@ public class Feed extends Controller {
 
     }
 
+    public static Promise<JsonNode> getMetadata(String url) {
+        String api = getApiBaseUrl() + "metadata/";
+
+        WSRequestHolder holder = WS
+                .url(api)
+                .setQueryParameter("url", url);
+        return holder.get().map(response -> response.asJson());
+    }
+
     @SecuredAction
     public static Result list() {
         User user = (User) ctx().args.get(SecureSocial.USER_KEY);
-        return ok(feed.render(user, getTweets(), getSyndFeeds()));
+
+        List<SyndEntry> entries = getSyndFeeds();
+        List<Item> items = new ArrayList<>();
+        for (SyndEntry entry : entries) {
+            Promise<JsonNode> metadataJson = getMetadata(entry.getLink());
+            items.add(new Item(entry, metadataJson.get(1000).get("content").toString()));
+        }
+
+        return ok(feed.render(user, getTweets(), items));
     }
+
+
 }
